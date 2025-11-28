358dad35240a08432a276fd5b41e1872d3a65fbe
JACOBO VIDAL GHAFFAR <jvidalghaffar@asepeyo.es>
2025-11-28 08:10:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8322369958-3ddd9",
+  "appId": "1:654202053280:web:45f19972d78c45b9b65fc5",
+  "apiKey": "AIzaSyDZwSa5gdbbRmWMSJ_K_HoK_Yii6DjXEh8",
+  "authDomain": "studio-8322369958-3ddd9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "654202053280"
+};
